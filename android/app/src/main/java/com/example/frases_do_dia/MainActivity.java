275d7435eec38e2a24ca4c1f369d2d32689a1925
Gustavo Mendes <gustamms@hotmail.com>
2020-02-19 22:55:10 -0300

    Criacao_app

---
+++ android/app/src/main/java/com/example/frases_do_dia/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.frases_do_dia;
+
+import android.support.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
